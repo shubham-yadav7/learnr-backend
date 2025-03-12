a740597e43b3b7d520d8c2b0a8c6021de5cbdd0c
Shubham Yadav <iamshubham1358@gmail.com>
2025-03-12 14:07:58 +0530

    logs fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ connectDB();
 app.get("/", (req, res)=>{
     try {
         
-        res.status(200).send({message: "Hello from learnr", PORT: process.env.PORT, usage : process.cpuUsage(), RAM : process.memoryUsage()})
+        res.status(200).send({message: "Hello from learnrs", PORT: process.env.PORT, usage : process.cpuUsage(), RAM : process.memoryUsage()})
     } catch (err) {
         res.status(400).send({message: "Hello from learnr-backend", PORT: process.env.PORT, usage : process.cpuUsage(), RAM : process.memoryUsage()})
         
